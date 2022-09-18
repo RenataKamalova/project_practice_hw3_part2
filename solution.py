1fa3f51231beb6f4415e540e6ee1c5a2f8b4381b
RenataKamalova <rene.kamalova@gmail.com>
2022-09-18 13:37:09 +0300

    added new item to list in file

--- solution.py
+++ solution.py
@@ -1,3 +1,3 @@
 def is_allowed_food(food):
-    allowed_foods = ["Peper", "Pizza", "Tomato", "Fish"]
+    allowed_foods = ["Peper", "Pizza", "Tomato", "Fish","Apples"]
     return food in allowed_foods
